0814adf27d5a5928109d3dd0898ee33376dc6dfa
CatSheiles <cathysheiles@gmail.com>
2020-06-04 18:08:48 +1000

    origin

---
+++ my-gallery.js
@@ -0,0 +1,21 @@
+new Swiper(".swiper-container", {
+    speed: 400,
+    spaceBetween: 100,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+});
+
+
+//testing alt version for swiper update need to change html swiper css link and swiper js script code if using this
+//var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    //direction: 'horizontal',
+    //loop: true,
+   // navigation: {
+  //  nextEl: '.swiper-button-next',
+  //  prevEl: '.swiper-button-prev'
+  //  }
+//})
+
