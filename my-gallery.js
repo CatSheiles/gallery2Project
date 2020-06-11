72fcb9cb768eb70174d55a47b05900289aba33ba
CatSheiles <cathysheiles@gmail.com>
2020-06-11 15:01:53 +1000

    version adding box slider and logo

--- my-gallery.js
+++ my-gallery.js
@@ -1,6 +1,7 @@
 new Swiper(".swiper-container", {
     speed: 400,
     spaceBetween: 100,
+    effect: "cube",
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
@@ -19,3 +20,27 @@ new Swiper(".swiper-container", {
   //  }
 //})
 
+//popmotion throwing the catface
+const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector('.brand');
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, 'mousedown touchstart')
+  .start((e) => {
+    e.preventDefault();
+    pointer(ballXY.get()).start(ballXY);
+  });
+
+listen(document, 'mouseup touchend')
+  .start(() => {
+    spring({
+      from: ballXY.get(),
+      velocity: ballXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 200,
+      mass: 1,
+      damping: 10
+    }).start(ballXY);
+  });
